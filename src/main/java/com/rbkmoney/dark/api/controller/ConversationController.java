9255491051e478ae6968c0adcc33226fe6f929f5
vitaxa <vit1993@mail.ru>
2019-09-25 17:51:44 +0300

    add rest for conversation controller (#20)

--- src/main/java/com/rbkmoney/dark/api/controller/ConversationController.java
+++ src/main/java/com/rbkmoney/dark/api/controller/ConversationController.java
@@ -1,18 +1,21 @@
 package com.rbkmoney.dark.api.controller;
 
-import com.rbkmoney.damsel.messages.GetConversationResponse;
 import com.rbkmoney.dark.api.service.ConversationService;
 import com.rbkmoney.swag.messages.api.ConversationApi;
 import com.rbkmoney.swag.messages.model.ConversationResponse;
 import com.rbkmoney.swag.messages.model.GetConversationParams;
 import com.rbkmoney.swag.messages.model.SaveConversationParams;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@RestController
+@RequiredArgsConstructor
 public class ConversationController implements ConversationApi {
 
-    private ConversationService conversationService;
+    private final ConversationService conversationService;
 
     @Override
     public ResponseEntity<ConversationResponse> getConversations(@Valid GetConversationParams getConversationParams) {
