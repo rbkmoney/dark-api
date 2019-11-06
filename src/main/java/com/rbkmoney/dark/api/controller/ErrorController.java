c5b7e55e50edc56750fecde464454928ef43a480
vitaxa <vitaxa93gamebox@gmail.com>
2019-11-06 12:59:18 +0300

    media type error handler (#36)

--- src/main/java/com/rbkmoney/dark/api/controller/ErrorController.java
+++ src/main/java/com/rbkmoney/dark/api/controller/ErrorController.java
@@ -3,8 +3,14 @@ package com.rbkmoney.dark.api.controller;
 import com.rbkmoney.dark.api.domain.ErrorResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,8 +18,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.context.request.WebRequest;
 
 import java.net.http.HttpTimeoutException;
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
@@ -31,6 +39,33 @@ public class ErrorController {
                 .message(e.getCause().getMessage())
                 .build();
     }
+
+    @ExceptionHandler({
+            HttpMediaTypeNotSupportedException.class,
+            HttpMediaTypeNotAcceptableException.class,
+    })
+    public final ResponseEntity<ErrorResponse> handleBadMediaType(Exception e, WebRequest request) throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        log.error("Bad MediaType", e);
+        if (e instanceof HttpMediaTypeNotSupportedException) {
+            HttpStatus status = HttpStatus.UNSUPPORTED_MEDIA_TYPE;
+            List<MediaType> mediaTypes = ((HttpMediaTypeNotSupportedException) e).getSupportedMediaTypes();
+            if (!CollectionUtils.isEmpty(mediaTypes)) {
+                headers.setAccept(mediaTypes);
+            }
+            ErrorResponse errorResp = ErrorResponse.builder()
+                    .code(INVALID_REQUEST)
+                    .message(e.getMessage())
+                    .build();
+            return new ResponseEntity<>(errorResp, headers, status);
+        } else if (e instanceof HttpMediaTypeNotAcceptableException) {
+            HttpStatus status = HttpStatus.NOT_ACCEPTABLE;
+            return new ResponseEntity<>(null, headers, status);
+        } else {
+            throw e;
+        }
+    }
+
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
