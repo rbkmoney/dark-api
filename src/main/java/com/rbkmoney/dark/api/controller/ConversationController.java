2b3e6f94a3b6d29f6d34c0fb48a98937a3d7f47e
vitaxa <vitaxa93gamebox@gmail.com>
2019-11-14 14:52:14 +0300

    bump swag-messages (#41)

--- src/main/java/com/rbkmoney/dark/api/controller/ConversationController.java
+++ src/main/java/com/rbkmoney/dark/api/controller/ConversationController.java
@@ -2,8 +2,8 @@ package com.rbkmoney.dark.api.controller;
 
 import com.rbkmoney.dark.api.service.ConversationService;
 import com.rbkmoney.swag.messages.api.ConversationApi;
-import com.rbkmoney.swag.messages.model.ConversationFilter;
 import com.rbkmoney.swag.messages.model.ConversationResponse;
+import com.rbkmoney.swag.messages.model.ConversationStatus;
 import com.rbkmoney.swag.messages.model.SaveConversationParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +21,8 @@ public class ConversationController implements ConversationApi {
 
     @Override
     public ResponseEntity<ConversationResponse> getConversations(@NotNull @Valid List<String> conversationId,
-                                                                 @Valid ConversationFilter filter) {
-        ConversationResponse conversation = conversationService.getConversation(conversationId, filter);
+                                                                 @Valid ConversationStatus conversationStatus) {
+        ConversationResponse conversation = conversationService.getConversation(conversationId, conversationStatus);
         return ResponseEntity.ok(conversation);
     }
 
