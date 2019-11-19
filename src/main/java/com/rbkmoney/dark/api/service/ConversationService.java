bd2716683dc741a58b9b63a761cceb2deb78f5c6
vitaxa <vitaxa93gamebox@gmail.com>
2019-11-19 16:35:58 +0300

    send user info for conversation save (#44)

--- src/main/java/com/rbkmoney/dark/api/service/ConversationService.java
+++ src/main/java/com/rbkmoney/dark/api/service/ConversationService.java
@@ -2,14 +2,14 @@ package com.rbkmoney.dark.api.service;
 
 import com.rbkmoney.damsel.messages.*;
 import com.rbkmoney.dark.api.converter.SwagConvertManager;
-import com.rbkmoney.dark.api.exceptions.ConversationUsersNotProvided;
+import com.rbkmoney.swag.messages.model.ConversationParam;
 import com.rbkmoney.swag.messages.model.ConversationResponse;
-import com.rbkmoney.swag.messages.model.SaveConversationParams;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.TException;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,18 +22,27 @@ public class ConversationService {
 
     private final SwagConvertManager swagConvertManager;
 
-    public void saveConversation(SaveConversationParams conversationParams) {
-        List<Conversation> conversationList = conversationParams.getConversations().stream()
-                .map(conversation -> swagConvertManager.convertToThrift(conversation, Conversation.class))
-                .collect(Collectors.toList());
-        List<User> userList = conversationParams.getUsers().stream()
-                .map(user -> swagConvertManager.convertToThrift(user, User.class))
+    public void saveConversation(List<ConversationParam> conversationParams, User user) {
+        String createdTime = Instant.now().toString();
+        List<Conversation> conversationList = conversationParams.stream()
+                .map(conversationParam -> {
+                    Conversation conversation = new Conversation();
+                    conversation.setConversationId(conversationParam.getConversationId());
+                    List<Message> messageList = conversationParam.getMessages().stream()
+                            .map(message -> {
+                                return new Message()
+                                        .setMessageId(message.getMessageId())
+                                        .setText(message.getText())
+                                        .setUserId(user.getUserId())
+                                        .setTimestamp(createdTime);
+                            })
+                            .collect(Collectors.toList());
+                    conversation.setMessages(messageList);
+                    return conversation;
+                })
                 .collect(Collectors.toList());
         try {
-            messageServiceSrv.saveConversations(conversationList, userList);
-        } catch (UsersNotProvided e) {
-            log.error("Not all user provided", e);
-            throw new ConversationUsersNotProvided(e.getIds());
+            messageServiceSrv.saveConversations(conversationList, user);
         } catch (TException e) {
             log.error("Save conversation failed", e);
             throw new RuntimeException(e);
@@ -49,7 +58,7 @@ public class ConversationService {
             GetConversationResponse conversations = messageServiceSrv.getConversations(conversationIdList, conversationFilter);
             return swagConvertManager.convertFromThrift(conversations, ConversationResponse.class);
         } catch (ConversationsNotFound e) {
-            log.error("Conversation not found", e);
+            log.error("Conversation not found: {}", e.getIds(), e);
             throw new IllegalArgumentException(e);
         } catch (TException e) {
             log.error("Get conversation failed", e);
