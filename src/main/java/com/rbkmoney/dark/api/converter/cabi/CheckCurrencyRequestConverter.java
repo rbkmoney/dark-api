0a960c7a9b0e42c0fd6a03a70a46d4188c836f60
vitaxa <v.banin@rbkmoney.com>
2020-02-18 18:55:09 +0300

    fix

--- src/main/java/com/rbkmoney/dark/api/converter/cabi/CheckCurrencyRequestConverter.java
+++ src/main/java/com/rbkmoney/dark/api/converter/cabi/CheckCurrencyRequestConverter.java
@@ -3,7 +3,6 @@ package com.rbkmoney.dark.api.converter.cabi;
 import com.rbkmoney.cabi.CheckCurrencyExchangeParams;
 import com.rbkmoney.cabi.Currency;
 import com.rbkmoney.cabi.ExchangeAction;
-import com.rbkmoney.cabi.base.Rational;
 import com.rbkmoney.dark.api.converter.ThriftConverter;
 import com.rbkmoney.dark.api.converter.ThriftConverterContext;
 import com.rbkmoney.dark.api.model.CabiCheckCurrencyRequestDto;
@@ -15,19 +14,11 @@ public class CheckCurrencyRequestConverter implements ThriftConverter<CheckCurre
 
     @Override
     public CheckCurrencyExchangeParams toThrift(CabiCheckCurrencyRequestDto value, ThriftConverterContext ctx) {
-        CheckCurrencyExchangeParams checkCurrencyExchangeParams = new CheckCurrencyExchangeParams();
-
-        Currency to = new Currency(value.getTo().getSymbolicCode(), value.getTo().getExponent());
-        Currency from = new Currency(value.getFrom().getSymbolicCode(), value.getFrom().getExponent());
-        checkCurrencyExchangeParams.setExchangeTo(to);
-        checkCurrencyExchangeParams.setExchangeFrom(from);
-
-        Rational amount = MathUtils.covertToRational(value.getAmount());
-        checkCurrencyExchangeParams.setAmount(amount);
-        ExchangeAction exchangeAction = extractExchangeAction(value.getAction());
-        checkCurrencyExchangeParams.setAction(exchangeAction);
-
-        return checkCurrencyExchangeParams;
+        return new CheckCurrencyExchangeParams(
+                new Currency(value.getTo().getSymbolicCode(), value.getTo().getExponent()),
+                new Currency(value.getFrom().getSymbolicCode(), value.getFrom().getExponent()),
+                extractExchangeAction(value.getAction()),
+                MathUtils.covertToRational(value.getAmount()));
     }
 
     private ExchangeAction extractExchangeAction(com.rbkmoney.swag.cabi.model.ExchangeAction exchangeAction) {
