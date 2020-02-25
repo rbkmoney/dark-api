76fc178fab011a323e07dc89935c954fa34cc5c6
vitaxa <vitaxa93gamebox@gmail.com>
2020-02-25 14:46:08 +0300

    BJ-802: fix crypto field exponent (#71)

--- src/test/java/com/rbkmoney/dark/api/controller/CabiControllerTest.java
+++ src/test/java/com/rbkmoney/dark/api/controller/CabiControllerTest.java
@@ -1,5 +1,13 @@
 package com.rbkmoney.dark.api.controller;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.json.JsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import com.jayway.jsonpath.spi.mapper.MappingProvider;
 import com.rbkmoney.cabi.CheckCurrencyExchangeParams;
 import com.rbkmoney.cabi.CryptoApiSrv;
 import com.rbkmoney.cabi.CurrencyRequestFail;
@@ -13,6 +21,7 @@ import com.rbkmoney.dark.api.service.KeycloakService;
 import com.rbkmoney.dark.api.service.PartyManagementService;
 import org.apache.thrift.TException;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +32,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.EnumSet;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -55,6 +66,35 @@ public class CabiControllerTest {
 
     @MockBean
     private DominantService dominantService;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        // work with double value as BigDecimal
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(DeserializationFeature.USE_LONG_FOR_INTS);
+        objectMapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
+
+        Configuration.setDefaults(new Configuration.Defaults() {
+
+            private final JsonProvider jsonProvider = new JacksonJsonProvider(objectMapper);
+            private final MappingProvider mappingProvider = new JacksonMappingProvider(objectMapper);
+
+            @Override
+            public JsonProvider jsonProvider() {
+                return jsonProvider;
+            }
+
+            @Override
+            public MappingProvider mappingProvider() {
+                return mappingProvider;
+            }
+
+            @Override
+            public Set<Option> options() {
+                return EnumSet.noneOf(Option.class);
+            }
+        });
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -90,6 +130,19 @@ public class CabiControllerTest {
                 ExchangeAction.buy);
 
         thenPerformCurrencyRequest("1.018093", null, 1.00, "9822.2894", "BTC", "USD", "BUY");
+    }
+
+    @Test
+    public void checkCurrencyExchangeBuyRubTest() throws Exception {
+        whenCurrencyExchangeResponse(new Rational(101654, 10),
+                new Rational(6546496651L, 10000),
+                new Rational(15528, 1000000),
+                new Rational(15428, 1000000),
+                "RUR",
+                "BTC",
+                ExchangeAction.buy);
+
+        thenPerformCurrencyRequest("10165.40", "0.015528", 0.02, "654649.6651", "RUR", "BTC", "BUY");
     }
 
     @Test
@@ -156,6 +209,8 @@ public class CabiControllerTest {
                 .andExpect(jsonPath("$.amountExchange").value(amountExchange));
         if (cryptoCurrencyAmountWithFee == null) {
             resultActions.andExpect(jsonPath("$.cryptoCurrencyAmountWithFee").doesNotExist());
+        } else {
+            resultActions.andExpect(jsonPath("$.cryptoCurrencyAmountWithFee").value(cryptoCurrencyAmountWithFee));
         }
         resultActions.andExpect(jsonPath("$.amount").value(amount))
                 .andExpect(jsonPath("$.rate").value(rate))
