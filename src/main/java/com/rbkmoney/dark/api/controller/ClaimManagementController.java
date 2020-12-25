c17261fa5f86d79d1c8931fc16bf142207d817ac
Pavel Popov <popoffpavel@yandex.ru>
2020-12-25 15:36:31 +0300

    Fix cm path, fix woody version

--- src/main/java/com/rbkmoney/dark/api/controller/ClaimManagementController.java
+++ src/main/java/com/rbkmoney/dark/api/controller/ClaimManagementController.java
@@ -18,6 +18,7 @@ import org.apache.thrift.TException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -32,6 +33,7 @@ import static com.rbkmoney.dark.api.util.ExceptionUtils.darkApi5xxException;
 
 @Slf4j
 @RestController
+@RequestMapping("")
 @RequiredArgsConstructor
 public class ClaimManagementController implements ProcessingApi {
 
